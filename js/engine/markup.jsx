b3d01fff92e87b8d5a497f12b7924c205bf54283
Dmitriy Stetsenko <sthetz.d@gmail.com>
2016-03-09 14:45:08 +0300

    fix URL regex

--- js/engine/markup.jsx
+++ js/engine/markup.jsx
@@ -1,5 +1,6 @@
-import URL_REGEXP from "./url_regexp.jsx"
 import Helpers    from "../libs/helpers.jsx"
+
+const URL_REGEXP = /[-a-zA-Z0-9@:%_\+.~#?&amp;\/\/=]{2,256}\.?[a-z]{2,4}\b([\/:][-a-zA-Z0-9@:%_\+.~#?&amp;\/\/=!]*){1,}/mg
 
 class Markup {
   constructor () {
